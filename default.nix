d6c03145b11d5ffdf5e65eced00eea4c0f3a3685
David Reiss <dnr@dnr.im>
2024-06-16 17:42:33 +0100

    initial ci build config

---
+++ default.nix
@@ -0,0 +1,154 @@
+# Stripped-down version of my (dnr)'s basic nix config:
+
+{ lib, config, pkgs, ... }:
+{
+  # latest kernel with styx config:
+  # FIXME: replace this with reference to styx module
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPatches = [ {
+    name = "styx";
+    patch = null;
+    extraStructuredConfig = {
+      CACHEFILES_ONDEMAND = lib.kernel.yes;
+      EROFS_FS_ONDEMAND = lib.kernel.yes;
+    };
+  } ];
+
+
+  # just enough to make nix-build not complain:
+  fileSystems."/".device = "/dev/dummy";
+  boot.loader.grub.device = "nodev";
+
+  hardware.enableRedistributableFirmware = true;
+  networking.networkmanager.enable = true;
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [
+    (import ./overlay-xcursor.nix)
+  ];
+
+  environment.systemPackages = with pkgs;
+  let
+    pythonWithMyPkgs = python3.withPackages (pp: with pp; [
+      dbus-python
+      pyserial
+      requests
+      websocket_client
+    ]);
+  in [
+
+    ascii
+    bc
+    binutils-unwrapped
+    borgbackup
+    brotli
+    btrfs-progs
+    compsize
+    cryptsetup
+    curl
+    darktable
+    ddcutil
+    diffstat
+    diffutils
+    direnv
+    dmenu
+    docker
+    docker-compose
+    dunst
+    easyeffects
+    evince
+    ffmpeg
+    file
+    gdb
+    gh
+    gimp
+    git
+    git-absorb
+    gnome-icon-theme
+    gnupg
+    gocryptfs
+    google-chrome
+    guvcview
+    hdparm
+    hugin
+    imagemagick
+    jq
+    libnotify
+    libsecret
+    lm_sensors
+    lsof
+    ltrace
+    luminanceHDR
+    lzma
+    magic-wormhole
+    mercurial
+    moreutils
+    mplayer
+    nix-direnv
+    nixos-option
+    nixpkgs-fmt
+    nodejs
+    notion
+    nvme-cli
+    openssh
+    openssl
+    opusTools
+    pavucontrol
+    pciutils
+    pipewire
+    psmisc
+    pulseaudio
+    pv
+    pythonWithMyPkgs
+    redshift
+    ripgrep
+    rsync
+    screen
+    scrot
+    smem
+    socat
+    spotify
+    sqlite
+    starship
+    strace
+    sxiv
+    sysstat
+    tcpdump
+    tig
+    tree
+    unzip
+    usbutils
+    vim
+    wget
+    wireguard-tools
+    wireplumber
+    xdotool
+    xdragon
+    xosd
+    xsel
+    xsettingsd
+    xxd
+    zip
+    zoom-us
+    zoxide
+    zstd
+
+  ];
+
+  services.fprintd.enable = true;
+  services.fwupd.enable = true;
+  services.tlp.enable = true;
+  services.xserver.enable = true;
+  services.zerotierone.enable = true;
+
+  fonts.packages = [
+    pkgs.noto-fonts
+    pkgs.noto-fonts-cjk
+    pkgs.noto-fonts-emoji
+    pkgs.ubuntu_font_family
+    (pkgs.nerdfonts.override { fonts = [ "UbuntuMono" ]; })
+  ];
+
+  documentation.nixos.enable = false;
+
+  system.stateVersion = "20.09";
+}
---
+++ overlay-xcursor.nix
@@ -0,0 +1,77 @@
+final: prev: {
+  # Allow xterm to load Xcursor lib to load cursors.
+  #
+  # The problem is that libX11 does a dlopen on "libXcursor.so.1" to load the Xcursor library,
+  # which uses the r[un]path of libX11 itself, so messing with xterm's r[un]path can't fix it.
+  # Messing with libX11 will require rebuilding way more than we want. LD_PRELOAD works but
+  # that's horrible and inherited by children of xterm. But: if we dlopen by absolute path first,
+  # then X11's dlopen by soname will find it.
+  #
+  # Note: for this to work with custom cursors, you also need to set XCURSOR_PATH in your
+  # environment, e.g.:
+  #   export XCURSOR_PATH=$(echo $XDG_DATA_DIRS | sed -E 's,(:|$),/icons&,g')
+  #
+  # references:
+  # https://github.com/NixOS/nixpkgs/issues/24137
+  # https://github.com/NixOS/nixpkgs/pull/101064
+  # https://cgit.freedesktop.org/xorg/lib/libX11/tree/src/CrGlCur.c#n74
+  xterm = prev.xterm.overrideAttrs (old: {
+    patches = old.patches ++ [
+      (final.writeText "xterm-load-xcursor.patch" ''
+        --- a/main.c
+        +++ b/main.c
+        @@ -94,4 +94,5 @@
+         #include <graphics.h>
+
+        +#include <dlfcn.h>
+         #if OPT_TOOLBAR
+
+        @@ -2510,4 +2512,6 @@ main(int argc, char *argv[]ENVP_ARG)
+         #endif /* } TERMIO_STRUCT */
+
+        +    // dlopen once here so it's cached when libX11 tries to dlopen it
+        +    dlopen("${final.xorg.libXcursor}/lib/libXcursor.so.1", RTLD_LAZY);
+             /* Init the Toolkit. */
+             {
+        '')
+    ];
+  });
+
+  # xvfb-run (depended on by libadwaita, etc.) has a build-time dep on xterm, only used in an
+  # installCheckPhase. So overriding xterm everywhere would cause a lot of rebuilds. This is an
+  # easy hack to prevent that.
+  xvfb-run = prev.xvfb-run.override { xterm = prev.xterm; };
+
+  # notion depends on xterm directly and requires a rebuild anyway, so might as
+  # well fix it too.
+  notion = prev.notion.overrideAttrs (old: {
+    patches = [
+      (final.writeText "notion-load-xcursor.patch" ''
+        --- a/notion/notion.c
+        +++ b/notion/notion.c
+        @@ -17,4 +17,5 @@
+         #include <sys/stat.h>
+         #include <fcntl.h>
+        +#include <dlfcn.h>
+
+         #include <libtu/util.h>
+        @@ -155,4 +156,7 @@ int main(int argc, char*argv[])
+             bool nodefaultconfig=FALSE;
+
+        +    // dlopen once here so it's cached when libX11 tries to dlopen it
+        +    dlopen("${final.xorg.libXcursor}/lib/libXcursor.so.1", RTLD_LAZY);
+        +
+             libtu_init(argv[0]);
+
+        '')
+    ];
+  });
+
+  # Just use LD_PRELOAD here since we can't patch it.
+  zoom-us = prev.zoom-us.overrideAttrs (old: {
+    postFixup = builtins.replaceStrings
+      ["--prefix LD_LIBRARY_PATH"]
+      ["--set LD_PRELOAD ${final.xorg.libXcursor}/lib/libXcursor.so.1 --prefix LD_LIBRARY_PATH"]
+      old.postFixup;
+  });
+}
