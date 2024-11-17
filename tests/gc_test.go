be1180d6a66f579c03576abbb17fe9769e6a4e8e
David Reiss <dnr@dnr.im>
2024-11-17 14:15:34 -0800

    test gc with sharing

--- tests/gc_test.go
+++ tests/gc_test.go
@@ -1,11 +1,13 @@
 package tests
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/dnr/styx/daemon"
 	"github.com/dnr/styx/pb"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/unix"
 )
 
 var gcUnmounted = map[pb.MountState]bool{
@@ -57,5 +59,58 @@ func TestGc(t *testing.T) {
 	tb.nixHash(mp3)
 	tb.nixHash(mp5)
 	d2 := tb.debug()
+	require.Zero(t, d2.Stats.Sub(d1.Stats).TotalReqs())
+}
+
+// randomized test
+func TestGcShared(t *testing.T) {
+	tb := newTestBase(t)
+	tb.startAll()
+
+	fetch := []string{
+		"cd1nbildgzzfryjg82njnn36i4ynyf8h-bash-interactive-5.1-p16-man",
+		"8vyj9c6g424mz0v3kvzkskhvzhwj6288-bash-interactive-5.2-p15-man",
+		"v35ysx9k1ln4c6r7lj74204ss4bw7l5l-openssl-3.0.12-man",
+		"xd96wmj058ky40aywv72z63vdw9yzzzb-openssl-3.0.12-man",
+		"1fka6ngkrlmqkhix0gnnb19z58sr0yma-openssl-3.0.13-man",
+		"z2waz77lsh4pxs0jxgmpf16s7a3g7b7v-openssl-3.0.13-man",
+		"53qwclnym7a6vzs937jjmsfqxlxlsf2y-opusfile-0.12",
+		"kcyrz2y8si9ry5p8qkmj0gp41n01sa1y-opusfile-0.12",
+		"qa22bifihaxyvn6q2a6w9m0nklqrk9wh-opusfile-0.12",
+	}
+	// mount all
+	mps := make([]string, len(fetch))
+	for _, j := range rand.Perm(len(mps)) {
+		mps[j] = tb.mount(fetch[j])
+	}
+	// read all (different order)
+	for _, j := range rand.Perm(len(mps)) {
+		tb.nixHash(mps[j])
+	}
+	// unmount half
+	var remaining []string
+	for i, j := range rand.Perm(len(mps)) {
+		if i < len(mps)/2 {
+			tb.umount(fetch[j])
+		} else {
+			remaining = append(remaining, mps[j])
+		}
+	}
+	mps = remaining
+
+	gc := tb.gc(daemon.GcReq{GcByState: gcUnmounted})
+	t.Log("gc:", gc)
+
+	unix.Sync()
+	tb.dropCaches()
+	unix.Sync()
+
+	// re-read remaining ones
+	d1 := tb.debug()
+	for _, mp := range mps {
+		tb.nixHash(mp)
+	}
+	d2 := tb.debug()
+	// no errors, no requests
 	require.Zero(t, d2.Stats.Sub(d1.Stats).TotalReqs())
 }
