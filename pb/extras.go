516003655cfb699b930c2273f300b333f5980ef6
David Reiss <dnr@dnr.im>
2024-11-18 19:52:09 -0800

    proto

--- pb/extras.go
+++ pb/extras.go
@@ -6,3 +6,17 @@ func (e *Entry) FileMode() int {
 	}
 	return 0o644
 }
+
+func (e *Entry) DigestBytesDef() int {
+	if e.DigestBytes == 0 {
+		return 24
+	}
+	return int(e.DigestBytes)
+}
+
+func (e *Entry) ChunkShiftDef() int {
+	if e.ChunkShift == 0 {
+		return 16
+	}
+	return int(e.ChunkShift)
+}
