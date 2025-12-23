61573bce8a6dbf6e449e096b202c23f63ddbc9af
David Reiss <dnr@dnr.im>
2025-12-23 15:25:55 -0800

    cobrautil: filters can return generic actions

---
+++ common/cobrautil/context.go
@@ -0,0 +1,41 @@
+package cobrautil
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/spf13/cobra"
+)
+
+type ckey struct {
+	t reflect.Type
+	k any
+}
+
+// Store stores a value in the Command's context.
+func Store[T any](c *cobra.Command, v T) {
+	c.SetContext(context.WithValue(c.Context(), ckey{t: reflect.TypeFor[T]()}, v))
+}
+
+// Get gets a value from the Command's context.
+func Get[T any](c *cobra.Command) T {
+	return c.Context().Value(ckey{t: reflect.TypeFor[T]()}).(T)
+}
+
+// Store stores a value in the Command's context with an arbitrary key.
+func StoreKeyed[T any](c *cobra.Command, v T, key any) {
+	c.SetContext(context.WithValue(c.Context(), ckey{t: reflect.TypeFor[T](), k: key}, v))
+}
+
+// Get gets a value from the Command's context with an arbitrary key.
+func GetKeyed[T any](c *cobra.Command, key any) T {
+	return c.Context().Value(ckey{t: reflect.TypeFor[T](), k: key}).(T)
+}
+
+// Storer returns a function that stores the given argument in the context at run time.
+func Storer[T any](v T) RunE {
+	return func(c *cobra.Command, ignored []string) error {
+		Store(c, v)
+		return nil
+	}
+}
