429c6d2b91c3fd3c93042ba56abd586f13943689
David Reiss <dnr@dnr.im>
2026-08-29 15:36:58 -0400

    fork github.com/pojntfx/go-nbd to fix error handling

--- default.nix
+++ default.nix
@@ -19,10 +19,10 @@ rec {
   baseArgs = {
     pname = "styx";
     inherit version;
-    vendorHash = "sha256-zxJRkb6pT69MnTmhZ54mI2EucBBFc8/GYqUGF6lI9kQ=";
+    vendorHash = "sha256-JwhgXsi9TmdecMTJF2YD7oEZeMrafa0I9EiBIfzR1Gw=";
     src = pkgs.lib.sourceByRegex ./. [
       "^go\\.(mod|sum)$"
-      "^(ci|cmd|common|daemon|erofs|manifester|pb|keys|tests)($|/.*)"
+      "^(ci|cmd|common|daemon|erofs|forked|manifester|pb|keys|tests)($|/.*)"
     ];
     subPackages = [ "cmd/styx" ];
     doCheck = false;
---
+++ forked/github.com/pojntfx/go-nbd/go.mod
@@ -0,0 +1,3 @@
+module github.com/pojntfx/go-nbd
+
+go 1.20
