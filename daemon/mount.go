448aea7da56e4e192ac34149f4560d0d4cc40a02
David Reiss <dnr@dnr.im>
2026-08-29 15:04:09 -0400

    split up daemon.go more

---
+++ daemon/mount.go
@@ -0,0 +1,136 @@
+package daemon
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/dnr/styx/erofs"
+	"github.com/dnr/styx/pb"
+	"go.etcd.io/bbolt"
+	"golang.org/x/sys/unix"
+	"google.golang.org/protobuf/proto"
+)
+
+// on-demand mount management
+
+func (s *Server) tryMount(ctx context.Context, req *MountReq) error {
+	_, sphStr, _ := ParseSph(req.StorePath)
+
+	path := filepath.Join(s.cfg.CachePath, "image", sphStr)
+
+	var imagePrefix []byte
+	if f, err := os.Open(path); err == nil {
+		// if we have an image we can proceed right to mounting
+		imagePrefix, err = io.ReadAll(io.LimitReader(f, 4096))
+		if err != nil {
+			f.Close()
+			return err
+		}
+		f.Close()
+	} else {
+		// if no image yet, get the manifest and build it
+		_, image, err := s.getManifestAndBuildImage(ctx, req)
+		if err != nil {
+			return err
+		}
+		if err = os.WriteFile(path+".tmp", image, 0o644); err != nil {
+			os.Remove(path + ".tmp")
+			return err
+		} else if err = os.Rename(path+".tmp", path); err != nil {
+			os.Remove(path + ".tmp")
+			return err
+		}
+		imagePrefix = image[:4096]
+	}
+
+	// do real mount
+	var mountErr error
+	isBare := erofs.IsBare(imagePrefix)
+	if isBare {
+		// set up empty file on target mount point
+		if st, err := os.Lstat(req.MountPoint); err != nil || !st.Mode().IsRegular() {
+			if err = os.RemoveAll(req.MountPoint); err != nil {
+				return fmt.Errorf("error clearing mount point for bare file: %w", err)
+			} else if err = os.WriteFile(req.MountPoint, nil, 0o644); err != nil {
+				return fmt.Errorf("error creating mount point for bare file: %w", err)
+			}
+		}
+		// mount to private dir
+		privateMp := filepath.Join(s.cfg.CachePath, "bare", sphStr)
+		_ = os.MkdirAll(privateMp, 0o755)
+		mountErr = unix.Mount(path, privateMp, "erofs", 0, "")
+		if mountErr == nil {
+			// now bind the bare file where it should go
+			mountErr = unix.Mount(privateMp+erofs.BarePath, req.MountPoint, "none", unix.MS_BIND, "")
+		}
+		// whether we succeeded or failed, unmount the original and clean up
+		_ = unix.Unmount(privateMp, 0)
+		_ = os.Remove(privateMp)
+	} else {
+		_ = os.MkdirAll(req.MountPoint, 0o755)
+		mountErr = unix.Mount(path, req.MountPoint, "erofs", 0, "")
+	}
+
+	_ = s.imageTx(sphStr, func(img *pb.DbImage) error {
+		if mountErr == nil {
+			img.MountState = pb.MountState_Mounted
+			img.LastMountError = ""
+		} else {
+			img.MountState = pb.MountState_MountError
+			img.LastMountError = mountErr.Error()
+		}
+		return nil
+	})
+
+	if mountErr != nil {
+		os.Remove(path) // force refetch/rebuild
+	}
+
+	return mountErr
+}
+
+func (s *Server) restoreMounts() {
+	var toRestore []*pb.DbImage
+	_ = s.db.View(func(tx *bbolt.Tx) error {
+		cur := tx.Bucket(imageBucket).Cursor()
+		for k, v := cur.First(); k != nil; k, v = cur.Next() {
+			var img pb.DbImage
+			if err := proto.Unmarshal(v, &img); err != nil {
+				log.Print("unmarshal error iterating images", string(k), err)
+				continue
+			}
+			// TODO: do this better
+			// if img.MountState == pb.MountState_MountError {
+			// 	log.Println("fixing", img.MountPoint)
+			// 	img.MountState = pb.MountState_Mounted
+			// 	img.ImageSize = 0
+			// 	toRestore = append(toRestore, &img)
+			// 	continue
+			// }
+			if img.MountState == pb.MountState_Mounted {
+				toRestore = append(toRestore, &img)
+			}
+		}
+		return nil
+	})
+	for _, img := range toRestore {
+		if mounted, err := isErofsMount(img.MountPoint); err == nil && mounted {
+			// log.Print("restoring: ", img.StorePath, " already mounted on ", img.MountPoint)
+			continue
+		}
+		err := s.tryMount(context.Background(), &MountReq{
+			StorePath:  img.StorePath,
+			MountPoint: img.MountPoint,
+			// the image has been written so we don't need upstream/narsize
+		})
+		if err == nil {
+			log.Print("restoring: ", img.StorePath, " restored to ", img.MountPoint)
+		} else {
+			log.Print("restoring: ", img.StorePath, " error: ", err)
+		}
+	}
+}
