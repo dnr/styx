704f7ad959533f39d9eb5fa79b10f9009c92175d
David Reiss <dnr@dnr.im>
2026-09-13 17:19:46 -0400

    nbd server: use pool

--- default.nix
+++ default.nix
@@ -19,7 +19,7 @@ rec {
   baseArgs = {
     pname = "styx";
     inherit version;
-    vendorHash = "sha256-nI5G1wkwRiXVMSiwYFYJqbNzBEqLy5PaFvMMAT2IgBQ=";
+    vendorHash = "sha256-UMqe/8b8ldxJ9zZ1aei/KQXMJskC+MGcYD4UsFALP58=";
     src = pkgs.lib.sourceByRegex ./. [
       "^go\\.(mod|sum)$"
       "^(ci|cmd|common|daemon|erofs|forked|manifester|pb|keys|tests)($|/.*)"
